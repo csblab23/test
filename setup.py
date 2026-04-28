3119199c82410cfea55819ce4f08ed28a141c63b
csblab23 <157005146+csblab23@users.noreply.github.com>
2026-04-28 15:09:44 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SCART",
+    version="0.1.0",
+    description="Single-cell Antigen Ranking Tool",
+    author="CSB LAB",
+    packages=find_packages(include=["SCART", "SCART.*"]),
+    python_requires=">=3.9,<3.12",
+    include_package_data=True,
+    package_data={
+        "SCART": [
+            "**/*.json",
+            "**/*.csv",
+            "**/*.txt",
+            "**/*.pkl",
+            "**/*.h5ad",
+            "**/*.yaml",
+            "**/*.yml",
+            "**/*.joblib",
+            "**/*.gmt",
+            "**/*.tsv"
+        ]
+    },
+    zip_safe=False,
+    install_requires=[
+        "scanpy==1.9.3",
+        "geofetch",
+        "GEOparse",
+        "popv==0.4.2",
+        "scvi-tools",
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "torch",
+        "tensorflow",
+        "deap==1.4",
+        "typer",
+        "rich",
+        "rpy2",
+        "onclass @ git+https://github.com/wangshenguiuc/OnClass.git"
+    ],
+    extras_require={
+        "copykat": [
+            "rpy2",
+        ]
+    }
+)
